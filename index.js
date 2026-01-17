572d98f7eb067a868dea98d19b58659b1d3444d5
Sandeep <sandeep.patel4488@gmail.com>
2026-01-17 13:46:30 +0530

    2. Rotate array to the right by k steps

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ function secondLargest(arr) {
 // Example
 console.log(secondLargest([10, 5, 20, 8, 18, 10, 20])); // 18
 
+
+// 2. Rotate array to the right by k steps
+function rotateArray(nums, k) {
+  const n = nums.length;
+  k = k % n; // handle k > n
+
+  reverse(nums, 0, n - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, n - 1);
+
+  return nums;
+}
+
+function reverse(arr, start, end) {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+}
+
+// Example
+console.log(rotateArray([1,2,3,4,5,6,7], 2));
