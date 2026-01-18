20458a0f17d09d52a3cd922898e26a403f397155
Sandeep <sandeep.patel4488@gmail.com>
2026-01-18 17:09:49 +0530

    4. Maximum Sum Subarray

--- index.js
+++ index.js
@@ -65,3 +65,19 @@ const count = removeDuplicates(data);
 
 console.log(data);
 console.log(count);
+
+// 4. Maximum Sum Subarray
+function maxSubArray(nums) {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+// Example
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // 6
