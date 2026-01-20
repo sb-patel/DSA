2e73e0f27e4de349d2812325b789386663293742
Sandeep <sandeep.patel4488@gmail.com>
2026-01-20 12:14:13 +0530

    7. Best Time to Buy and Sell Stock

--- index.js
+++ index.js
@@ -126,3 +126,25 @@ function maxArea(height) {
 
 // Example
 console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
+
+
+// 7. Best Time to Buy and Sell Stock
+
+function maxProfit(prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else {
+      const profit = prices[i] - minPrice;
+      maxProfit = Math.max(maxProfit, profit);
+    }
+  }
+
+  return maxProfit;
+}
+
+// Example
+console.log(maxProfit([7,1,5,3,6,4])); // 5
